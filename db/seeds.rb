5c73a3b5e774414edc5c855de7e6932f635394db
Ellen Lee <ellen811003@gmail.com>
2019-08-14 13:52:31 +0100

    completed restuarant challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Hoxton 100', address: 'Hoxton', phone_number: '07110000000', category: 'chinese')
+Restaurant.create(name: 'Itsu', address: 'Kings Cross', phone_number: '07110001234', category: 'japanese')
+Restaurant.create(name: 'Rome', address: 'Belsize Park', phone_number: '07230001234', category: 'italian')
+Restaurant.create(name: 'Belgium', address: 'Dalston', phone_number: '07340001234', category: 'belgian')
+Restaurant.create(name: 'Paris', address: 'Baker Street', phone_number: '07450001234', category: 'french')
